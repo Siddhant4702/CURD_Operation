e802abc2195430144bec1a16984e78f07a01ae1c
Siddhant Gaikwad <121213512+Siddhant4702@users.noreply.github.com>
2024-02-13 18:05:53 +0530

    Add files via upload
    Added only Controller, View, Model , appSettings.json, Program.cs and Startup.cs. Other files and folder need to  add while Creating Project

---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CURD_using_SDSclient.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Enter First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage ="Enter Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Email is Reqiured")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
